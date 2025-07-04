5950909449ece8353e4169909f0756609914878b
vanhassel-vincent <vincent.vanhassel@gmail.com>
2025-07-04 16:43:01 +0200

    update menu - Printemps-été

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -5,10 +5,10 @@
     <!-- Le contenu prend toute la hauteur dispo -->
     <main class="flex-grow pt-40 px-6 md:px-20">
       <div class="max-w-6xl mx-auto space-y-12">
-        <img src="/image-menu/menu-1.png" alt="Menu-1" class="rounded-lg shadow-md w-full object-contain" />
-        <img src="/image-menu/menu-2.png" alt="Menu-2" class="rounded-lg shadow-md w-full object-contain" />
-        <img src="/image-menu/menu-3.png" alt="Menu-3" class="rounded-lg shadow-md w-full object-contain" />
-        <img src="/image-menu/menu-4.png" alt="Menu-4" class="rounded-lg shadow-md w-full object-contain" />
+        <img src="/image-menu/Menu Printemps-Été.jpeg" alt="Menu-1" class="rounded-lg shadow-md w-full object-contain" />
+        <!-- <img src="/image-menu/menu-2.png" alt="Menu-2" class="rounded-lg shadow-md w-full object-contain" /> -->
+        <img src="/image-menu/Carte des boissons  Printemps-Été.jpeg" alt="Menu-3" class="rounded-lg shadow-md w-full object-contain" />
+        <!-- <img src="/image-menu/menu-4.png" alt="Menu-4" class="rounded-lg shadow-md w-full object-contain" /> -->
       </div>
     </main>
 
